9df7d4af4952768f90cfc9233dcaa247d60e284c
Peter Haas <peter@thegoodkitchen.com>
2021-06-28 20:32:14 -0400

    add basic auth

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 ### Import Packages ###
 import dash
+import dash_auth
 import dash_bootstrap_components as dbc
+
+# REMOVE AT DEPLOYMENT
+VALID_USERNAME_PASSWORD_PAIRS = {
+    'hello': 'world'
 
 ### Dash instance ###
 external_stylesheets = [dbc.themes.UNITED]
 app = dash.Dash(
-        __name__,
-        external_stylesheets=external_stylesheets,
-        )
+    __name__,
+    external_stylesheets=external_stylesheets,
+)
+auth = dash_auth.BasicAuth(
+    app,
+    VALID_USERNAME_PASSWORD_PAIRS,
+)
