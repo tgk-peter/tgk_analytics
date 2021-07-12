c33ab5366dcafbe37f2b1af68d55ce42e63db0b2
Peter Haas <peter@thegoodkitchen.com>
2021-07-12 07:31:11 -0400

    got daterangepicker to update dataframe and tables

--- pages/cancellations.py
+++ pages/cancellations.py
@@ -41,13 +41,14 @@ while "next" in result.links:
   if result.headers['X-Recharge-Limit'] == '39/40':
     time.sleep(0.5)
 '''
+
 ### Create DataFrames ###
 
 ## All data
 df = pd.json_normalize(total_results)
 
 ## DataFrame for view and time slice
-
+'''
 # Create a new dataframe that keeps customer id, when they cancelled,
 # the primary reason they cancelled, and the cancellation comments they left.
 columns = ["customer_id", "cancelled_at", "cancellation_reason",
@@ -55,6 +56,7 @@ columns = ["customer_id", "cancelled_at", "cancellation_reason",
 df_cancel = df.loc[:, columns]
 # Convert 'cancelled_at' values to datetime format.
 df_cancel["cancelled_at"] = pd.to_datetime(df_cancel["cancelled_at"])
+
 # Time slice
 cancelled_at_min = pd.Timestamp('now').floor('D') - pd.Timedelta(7, unit="D")
 cancelled_at_max = pd.Timestamp('today').floor('D')
@@ -71,7 +73,7 @@ reasons_not_empty = (df_cancel["cancellation_reason_comments"].notnull()) \
                     & (df_cancel["cancellation_reason_comments"] != "")
 df_cancel_reasons = df_cancel.loc[reasons_not_empty]
 df_cancel_reasons = df_cancel_reasons.sort_values(by="cancelled_at", ascending=False)
-
+'''
 ### Cancellation Layout and Callbacks ###
 app.layout = html.Div(
     children=[
@@ -79,46 +81,126 @@ app.layout = html.Div(
             children='Cancellations',
         ),
         dcc.DatePickerRange(
-            id='my-date-picker-range',
+            id='date-picker-range',
             min_date_allowed=date(2019, 6, 1),
             max_date_allowed=date.today(),
             initial_visible_month=date.today(),
-            start_date=date.today() - timedelta(days=7),
-            end_date=date.today(),
+#            start_date=date.today() - timedelta(days=7),
+#            end_date=date.today(),
+            start_date=pd.Timestamp('now').floor('D') - pd.Timedelta(7, unit="D"),
+            end_date=pd.Timestamp('today').floor('D'),
         ),
-        dbc.Table.from_dataframe(
-            df = df_cancel_counts,
-            id = "cancel_counts",
-            striped=True,
-            bordered=True,
-            hover=True,
-        ),
+        html.Div(id="cancel_counts_container"),
+#        dbc.Table.from_dataframe(
+#            df = df_cancel_counts,
+#            id = "cancel_counts",
+#            striped=True,
+#            bordered=True,
+#            hover=True,
+#        ),
         dcc.Markdown('''
             When provided, here are the cancellation reason comments:
         '''),
-        dbc.Table.from_dataframe(
-            df = df_cancel_reasons,
-            id = "cancel_reasons",
-            striped=True,
-            bordered=True,
-            hover=True,
-        ),
+        html.Div(id="cancel_reasons_container"),
+#        dbc.Table.from_dataframe(
+#            df = df_cancel_reasons,
+#            id = "cancel_reasons",
+#            striped=True,
+#            bordered=True,
+#            hover=True,
+#        ),
     ]
 )
-'''
+# Update cancel counts container with table
 @app.callback(
     Output(
-        component_id='table_counts',
-        component_property='df',
+        component_id='cancel_counts_container',
+        component_property='children',
     ),
     [Input(
-        component_id='page-2-radios',
-        component_property='value',
+        component_id='date-picker-range',
+        component_property='start_date',
+    ),
+    Input(
+        component_id='date-picker-range',
+        component_property='end_date',
     )]
 )
-def page_2_radios(value):
-    return 'You have selected "{}"'.format(value)
-'''
+def update_count_table(start_date, end_date):
+
+    # Create a new dataframe that keeps customer id, when they cancelled,
+    # the primary reason they cancelled, and the cancellation comments they left.
+    columns = ["customer_id", "cancelled_at", "cancellation_reason",
+                "cancellation_reason_comments"]
+    df_cancel = df.loc[:, columns]
+    # Convert 'cancelled_at' values to datetime format.
+    df_cancel["cancelled_at"] = pd.to_datetime(df_cancel["cancelled_at"])
+
+    # Time slice
+    cancelled_at_min = start_date
+    cancelled_at_max = end_date
+    date_range = (df_cancel["cancelled_at"] > cancelled_at_min)\
+            & (df_cancel["cancelled_at"] < cancelled_at_max)
+    df_cancel= df_cancel.loc[date_range]
+
+    ## DataFrame for cancellation value counts and rename columns
+    df_cancel_counts = df_cancel["cancellation_reason"].value_counts().to_frame().reset_index()
+    df_cancel_counts.rename(columns={"index":"Reason", "cancellation_reason":"Count"}, inplace=True)
+    return dbc.Table.from_dataframe(
+        df = df_cancel_counts,
+        id = "cancel_counts",
+        striped=True,
+        bordered=True,
+        hover=True,
+    )
+
+# Update cancel reasons table
+@app.callback(
+    Output(
+        component_id='cancel_reasons_container',
+        component_property='children',
+    ),
+    [Input(
+        component_id='date-picker-range',
+        component_property='start_date',
+    ),
+    Input(
+        component_id='date-picker-range',
+        component_property='end_date',
+    )]
+)
+def update_reason_table(start_date, end_date):
+    
+    # Create a new dataframe that keeps customer id, when they cancelled,
+    # the primary reason they cancelled, and the cancellation comments they left.
+    columns = ["customer_id", "cancelled_at", "cancellation_reason",
+                "cancellation_reason_comments"]
+    df_cancel = df.loc[:, columns]
+    # Convert 'cancelled_at' values to datetime format.
+    df_cancel["cancelled_at"] = pd.to_datetime(df_cancel["cancelled_at"])
+
+    # Time slice
+    cancelled_at_min = start_date
+    cancelled_at_max = end_date
+    date_range = (df_cancel["cancelled_at"] > cancelled_at_min)\
+            & (df_cancel["cancelled_at"] < cancelled_at_max)
+    df_cancel= df_cancel.loc[date_range]
+
+    ## Dataframe for non-empty reasons
+    reasons_not_empty = (df_cancel["cancellation_reason_comments"].notnull()) \
+                        & (df_cancel["cancellation_reason_comments"] != "")
+    df_cancel_reasons = df_cancel.loc[reasons_not_empty]
+    df_cancel_reasons = df_cancel_reasons.sort_values(by="cancelled_at", ascending=False)
+
+    ## Return table with DataFrame
+    return dbc.Table.from_dataframe(
+        df = df_cancel_reasons,
+        id = "cancel_reasons",
+        striped=True,
+        bordered=True,
+        hover=True,
+    )
+
 ### Development Server
 if __name__ == '__main__':
     app.run_server(debug=True)
