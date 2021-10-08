20c96d287b0292b2c3e2169bfafa754f17b609c2
Peter Haas <peter@thegoodkitchen.com>
2021-10-08 11:08:32 -0400

    create

---
+++ data_cache/update_active_table.py
@@ -0,0 +1,131 @@
+# update_active_table.py
+# Create db table from Shopify API to reduce execution time in Dash app.
+
+# Import Packages #
+
+import cryptpandas as crp
+from dotenv import load_dotenv
+from github import Github
+import os
+import pandas as pd
+import requests
+import time
+
+# Import .env variables #
+load_dotenv()  # take environment variables from .env
+CRP_PASSWORD = os.getenv('CRP_PASSWORD')
+SHOPIFY_PASSWORD = os.getenv('SHOPIFY_PASSWORD')
+GITHUB_ACCESS_TOKEN = os.getenv('GITHUB_ACCESS_TOKEN')
+
+#####################################
+# Get Data from Shopify Order API ###
+#####################################
+
+
+def get_shopify_order_api(endpoint, status):
+    '''Request and hold paginated data from the Shopify Order API
+
+    Keyword arguments:
+    status -- that status of the order('open', 'closed', 'cancelled', 'any')
+    endpoint -- the target Shopify endpoint
+
+    Reference:
+    https://shopify.dev/api/admin/rest/reference/orders/order
+    '''
+    # Set URL
+    headers = {
+        "X-Shopify-Access-Token": SHOPIFY_PASSWORD,
+        "Content-Type": "application/json"
+    }
+    shop = "the-good-kitchen-esc.myshopify.com"
+    endpoint = endpoint
+    status = status
+    limit = 250
+    fields = 'email,order_number,created_at,cancelled_at,line_items'
+    url = (f'https://{shop}/{endpoint}?fields={fields}&status={status}'
+           f'&limit={limit}')
+
+    # Access and store first page of results
+    session = requests.Session()
+    response = session.get(url, headers=headers)
+    response_data = response.json()
+    all_records = response_data['orders']
+
+    # While Next Link is present, access and store next page
+    count = 0
+    while "next" in response.links:
+        next_url = response.links["next"]["url"]
+        response = session.get(next_url, headers=headers)
+        count += 1
+        print(f'# of request cycles: {count}')
+        response_data = response.json()
+        all_records.extend(response_data['orders'])
+        # Sleep to avoid rate limit if approach bucket size
+        if response.headers['X-Shopify-Shop-Api-Call-Limit'] == '79/80':
+            time.sleep(0.25)
+
+    return all_records
+
+#########
+
+
+def generate_active_order_df(records, path):
+    '''Create, encrypt, store cancellation DataFrame
+
+    Keyword arguments:
+    records -- dictionary of records from API
+    path -- file path to write encrypted DataFrame
+    '''
+
+    # Generate df from json and trim it to target
+    # Keep active (non-cancelled) subscription orders.
+    # Keep customer email, order number, order creation date,
+    # subscription sku, and cancelled_at date.
+    # Convert created_at to datetime
+    df_orders_sub = pd.json_normalize(
+        records,
+        record_path='line_items',
+        meta=['email', 'order_number', 'created_at', 'cancelled_at'])
+    df_orders_sub = df_orders_sub.loc[:, ['email', 'order_number',
+                                          'created_at', 'sku', 'cancelled_at']]
+    df_orders_sub['created_at'] = pd.to_datetime(df_orders_sub['created_at'])
+    df_orders_sub['sku'].fillna('No SKU', inplace=True)
+    df_orders_sub = df_orders_sub[df_orders_sub['sku'].str.contains('SUB')]
+    df_orders_sub = df_orders_sub[df_orders_sub['cancelled_at'].isnull()]
+
+    # Encrypt and store the df locally
+    crp.to_encrypted(df_orders_sub, password=CRP_PASSWORD, path=path)
+
+
+def github_update(file_path):
+    '''Read file content and update in github repository
+
+    Keyword arguments:
+    file_path -- path of file to read and update
+    '''
+    with open(file_path) as file:
+        file_content = file.read()
+    github = Github(GITHUB_ACCESS_TOKEN)
+    repo = github.get_user().get_repo('tgk_analytics')
+    contents = repo.get_contents(file_path)
+    repo.update_file(
+        path=contents.path,
+        message='next commit',
+        content=file_content,
+        sha=contents.sha,
+    )
+
+# Get all active Shopify orders #
+
+
+active_orders = get_shopify_order_api(
+    endpoint='admin/api/2021-07/orders.json',
+    status='any',
+)
+generate_active_order_df(
+    records=active_orders,
+    path='data_cache/active_order_cache.crypt',
+)
+# github_update(
+#     file_path='data_cache/active_order_cache.crypt',
+# )
