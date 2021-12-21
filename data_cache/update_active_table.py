aa7b579a2d92ff99cbbe1d71f80c4597b95c51ae
Peter Haas <peter@thegoodkitchen.com>
2021-12-21 12:54:52 -0500

    add records sequential

--- data_cache/update_active_table.py
+++ data_cache/update_active_table.py
@@ -28,8 +28,35 @@ HEROKU_DB_URL = DATABASE_URL.replace('postgres://', 'postgresql://')
 #####################################
 
 
+def generate_active_order_df(records):
+    '''Create active order DataFrame
+
+    Keyword arguments:
+    records -- dictionary of records from API
+    '''
+
+    # Generate df from json and trim it to target
+    # Keep active (non-cancelled) subscription orders.
+    # Keep customer email, order number, order creation date,
+    # subscription sku, and cancelled_at date.
+    # Convert created_at to datetime
+    df_orders_sub = pd.json_normalize(
+        records,
+        record_path='line_items',
+        meta=['email', 'order_number', 'created_at', 'cancelled_at']
+    )
+    df_orders_sub = df_orders_sub.loc[:, ['email', 'order_number',
+                                          'created_at', 'sku', 'cancelled_at']]
+    df_orders_sub['created_at'] = pd.to_datetime(df_orders_sub['created_at'])
+    df_orders_sub['sku'].fillna('No SKU', inplace=True)
+    df_orders_sub = df_orders_sub[df_orders_sub['sku'].str.contains('SUB')]
+    df_orders_sub = df_orders_sub[df_orders_sub['cancelled_at'].isnull()]
+    return df_orders_sub
+
+
 def get_shopify_order_api(endpoint, status):
-    '''Request and hold paginated data from the Shopify Order API
+    '''Request paginated data from the Shopify Order API. Convert each page to
+    dataframe and add to database.
 
     Keyword arguments:
     status -- that status of the order('open', 'closed', 'cancelled', 'any')
@@ -51,73 +78,48 @@ def get_shopify_order_api(endpoint, status):
     url = (f'https://{shop}/{endpoint}?fields={fields}&status={status}'
            f'&limit={limit}')
 
-    # Access and store first page of results
+    # Access first page of results
     session = requests.Session()
     response = session.get(url, headers=headers)
     response_data = response.json()
-    all_records = response_data['orders']
+    records = response_data['orders']
+    active_order_df = generate_active_order_df(records)
 
-    # While Next Link is present, access and store next page
-    count = 0
+    # Replace database
+    engine = create_engine(HEROKU_DB_URL, echo=False)
+    active_order_df.to_sql('active_sub',
+                           con=engine,
+                           if_exists='replace',
+                           index=False,
+                           dtype={'created_at': DateTime()}
+                          )
+
+    # While Next Link is present, access next page and append to database
+    # count = 0
     while "next" in response.links:
         next_url = response.links["next"]["url"]
         response = session.get(next_url, headers=headers)
-        count += 1
+        # count += 1
         # print(f'# of request cycles: {count}')
         response_data = response.json()
-        all_records.extend(response_data['orders'])
+        records = response_data['orders']
+        active_order_df = generate_active_order_df(records)
+        active_order_df.to_sql('active_sub',
+                               con=engine,
+                               if_exists='append',
+                               index=False,
+                               dtype={'created_at': DateTime()}
+                              )
         # Sleep to avoid rate limit if approach bucket size
         if response.headers['X-Shopify-Shop-Api-Call-Limit'] == '79/80':
             time.sleep(0.25)
-
-    return all_records
-
-#########
-
-
-def generate_active_order_df(records):
-    '''Create and store active order DataFrame in database
-
-    Keyword arguments:
-    records -- dictionary of records from API
-    path -- file path to write encrypted DataFrame
-    '''
-
-    # Generate df from json and trim it to target
-    # Keep active (non-cancelled) subscription orders.
-    # Keep customer email, order number, order creation date,
-    # subscription sku, and cancelled_at date.
-    # Convert created_at to datetime
-    df_orders_sub = pd.json_normalize(
-        records,
-        record_path='line_items',
-        meta=['email', 'order_number', 'created_at', 'cancelled_at']
-    )
-    df_orders_sub = df_orders_sub.loc[:, ['email', 'order_number',
-                                          'created_at', 'sku', 'cancelled_at']]
-    df_orders_sub['created_at'] = pd.to_datetime(df_orders_sub['created_at'])
-    df_orders_sub['sku'].fillna('No SKU', inplace=True)
-    df_orders_sub = df_orders_sub[df_orders_sub['sku'].str.contains('SUB')]
-    df_orders_sub = df_orders_sub[df_orders_sub['cancelled_at'].isnull()]
-
-    # Convert DataFrame to sql and store in database
-    engine = create_engine(HEROKU_DB_URL, echo=False)
-    df_orders_sub.to_sql('active_sub',
-                         con=engine,
-                         if_exists='replace',
-                         index=False,
-                         dtype={'created_at': DateTime()}
-                         )
 
 ##########################################
 # Get active Shopify subscription orders #
 ##########################################
 
 
-all_orders = get_shopify_order_api(
+get_shopify_order_api(
     endpoint='admin/api/2021-07/orders.json',
     status='any',
-)
-generate_active_order_df(
-    records=all_orders
 )
